e4234b86dce4465efb9b1629fbfa6de56d2a0a24
BookStore <ivansmgo@gmail.com>
2021-04-07 10:07:41 +0200

    Updates

---
+++ precache-manifest.cfcd8e66770fe036d1361a2ce04a2a29.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "261cea6e0db9d403bd295d9a6ef5f376",
+    "url": "/tmpcotton/index.html"
+  },
+  {
+    "revision": "807b43e4afd9e1e79b46",
+    "url": "/tmpcotton/static/css/main.9cca392e.chunk.css"
+  },
+  {
+    "revision": "5344823ad83f3d3f9122",
+    "url": "/tmpcotton/static/js/2.32595d14.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/tmpcotton/static/js/2.32595d14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "807b43e4afd9e1e79b46",
+    "url": "/tmpcotton/static/js/main.d8f34f8b.chunk.js"
+  },
+  {
+    "revision": "e0986a49aa9faeb2fef0",
+    "url": "/tmpcotton/static/js/runtime-main.50ce0353.js"
+  },
+  {
+    "revision": "fa254f01a19f95e5f02f63677f0284b4",
+    "url": "/tmpcotton/static/media/cotton-min.fa254f01.png"
+  },
+  {
+    "revision": "f5ca48a7fa740d19c7acfa9f61d036e1",
+    "url": "/tmpcotton/static/media/forHerMin.f5ca48a7.jpg"
+  },
+  {
+    "revision": "b77800324b04ed021010f982a4e8e128",
+    "url": "/tmpcotton/static/media/forHerNew.b7780032.png"
+  },
+  {
+    "revision": "5b1d64abeeafdb787a3bd1d45c584785",
+    "url": "/tmpcotton/static/media/forHimImage.5b1d64ab.png"
+  },
+  {
+    "revision": "bc640ffd89230dc7da7262d6effaf9ed",
+    "url": "/tmpcotton/static/media/forHimMin.bc640ffd.jpg"
+  },
+  {
+    "revision": "7037fd2b1a3949d6cbc12d3620d7e799",
+    "url": "/tmpcotton/static/media/lady.7037fd2b.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0cad424df72ef70591bdf4d6dd521af.js"
+  "/tmpcotton/precache-manifest.cfcd8e66770fe036d1361a2ce04a2a29.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tmpcotton/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
